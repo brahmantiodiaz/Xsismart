8571d9c2e1d940e66c377e2b8713b3a9831e50d0
brahmantiodiaz28 <brahmantiodiaz28@gmail.com>
2021-03-24 08:49:45 +0700

    commit

--- frontend/src/Catalog/index.jsx
+++ frontend/src/Catalog/index.jsx
@@ -27,45 +27,46 @@ export default class index extends React.Component {
         const { List } = this.state
         return (
             <div>
-                {
-                            List.map(data => {
-                                return (
-                                    <div class="col-sm-3 card card-widget widget-user">
-                    <div class="widget-user-header bg-info">
-                        <h3 class="widget-user-username">{data.name}</h3>
-                        <h5 class="widget-user-desc">{data.price}</h5>
-                    </div>
-                    <div class="widget-user-image">
-                        <img class="img-circle elevation-2" src={Photo} alt="User Avatar" />
-                    </div>
-                    <div class="card-footer">
-                        <div class="row">
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <button type="button" class="btn btn-primary">-</button>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <h5 class="description-header">0</h5>
-                                </div>
-                            </div>
-                            <div class="col-sm-4 border-right">
-                                <div class="description-block">
-                                    <button type="button" class="btn btn-primary">+</button>
-                                </div>
+                <div class="row">
+                    {
+                        List.map(data => {
+                            return (
+                                <div class="col-sm-3 card card-widget widget-user">
+                                    <div class="widget-user-header bg-info">
+                                        <h3 class="widget-user-username">{data.name}</h3>
+                                        <h5 class="widget-user-desc">{data.price}</h5>
+                                    </div>
+                                    <div class="widget-user-image">
+                                        <img class="img-circle elevation-2" src={Photo} alt="User Avatar" />
+                                    </div>
+                                    <div class="card-footer">
+                                        <div class="row">
+                                            <div class="col-sm-4 border-right">
+                                                <div class="description-block">
+                                                    <button type="button" class="btn btn-primary">-</button>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4 border-right">
+                                                <div class="description-block">
+                                                    <h5 class="description-header">0</h5>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4 border-right">
+                                                <div class="description-block">
+                                                    <button type="button" class="btn btn-primary">+</button>
+                                                </div>
 
-                            </div>
+                                            </div>
 
-                        </div>
+                                        </div>
 
-                    </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
-                                )
-                            })
-                        }                
             </div>
-
         )
     }
 
